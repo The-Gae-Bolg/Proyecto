3be6f5b2dd619b41c272a03191adcf9c45577967
The-Gae-Bolg <65456792+The-Gae-Bolg@users.noreply.github.com>
2020-08-15 22:08:58 -0500

    archivos

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    
+    // Update is called once per frame
+    public float speed = 4f;
+    Animator anim;
+    Rigidbody2D rb2d;
+    Vector2 mov;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mov = new Vector2(
+            Input.GetAxisRaw("Horizontal"),
+            Input.GetAxisRaw("Vertical")
+        );
+
+        if (mov != Vector2.zero) {
+            anim.SetFloat("movX", mov.x);
+            anim.SetFloat("movY", mov.y);
+            anim.SetBool("walking", true);
+        } else {
+            anim.SetBool("walking", false);
+        }
+
+    }
+
+    void FixedUpdate () {
+        // Nos movemos en el fixed por las físicas
+        rb2d.MovePosition(rb2d.position + mov * speed * Time.deltaTime);
+    }
+
+}
