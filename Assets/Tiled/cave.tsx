950eaa54a5d29c382ef42e2c9ca152d45433fc67
Hectorreto <hdeandaf@hotmail.com>
2021-04-28 18:38:55 -0500

    Casa 2do Piso

---
+++ Assets/Tiled/cave.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="cave" tilewidth="16" tileheight="16" tilecount="1000" columns="40">
+ <image source="D:/Users/hdean/Documents/GitHub/Proyecto/Assets/Sprites/RPG/cave.png" width="640" height="400"/>
+</tileset>
