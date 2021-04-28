6ca833182715539e5deebce8a9507c58c67ac84b
Hectorreto <hdeandaf@hotmail.com>
2021-04-27 19:55:43 -0500

    Menú de pausa (ESC)

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (GameIsPaused) {
+                Resume();
+            }
+            else {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume() {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause() {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu() {
+        Debug.Log("Loading menu...");
+        // Time.timeScale = 1f
+        // Ir al menú
+    }
+
+    public void QuitGame() {
+        Debug.Log("Quitting game...");
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+}
