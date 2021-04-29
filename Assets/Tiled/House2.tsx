bddccb940657333f33e252d805b3071130415286
The-Gae-Bolg <65456792+The-Gae-Bolg@users.noreply.github.com>
2021-04-29 16:17:18 -0500

    segundo piso
    se agregaron decoraciones

---
+++ Assets/Tiled/House2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="House2" tilewidth="16" tileheight="16" tilecount="512" columns="32">
+ <image source="../Sprites/RPG/House.png" width="512" height="256"/>
+</tileset>
