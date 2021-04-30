94ef70eb25cafa27035d4dc8bbf81e6354142a48
Hectorreto <hdeandaf@hotmail.com>
2021-04-30 01:31:19 -0500

    Hitbox de ataque del jugador

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,32 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Assertions;
 
-public class Player : MonoBehaviour
-{
+public class Player : MonoBehaviour {
     
-    // Update is called once per frame
     public float speed = 4f;
+
     Animator anim;
     Rigidbody2D rb2d;
     Vector2 mov;
 
+    CircleCollider2D attackCollider;
+
     public GameObject initialMap;
 
-    void Awake(){
+    void Awake() {
         Assert.IsNotNull(initialMap);
     }
 
-    void Start()
-    {
+    void Start() {
         anim = GetComponent<Animator>();
         rb2d = GetComponent<Rigidbody2D>();
+
+        // Recuperamos el collider de ataque del primer hijo
+        attackCollider = transform.GetChild(0).GetComponent<CircleCollider2D>();
+        attackCollider.enabled = false;
 
         Camera.main.GetComponent<MainCamera>().SetBound(initialMap);
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         mov = new Vector2(
             Input.GetAxisRaw("Horizontal"),
             Input.GetAxisRaw("Vertical")
