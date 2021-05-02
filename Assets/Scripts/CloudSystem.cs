77737d0dc4faf029e93ddd748dc6e50b891c2732
The-Gae-Bolg <65456792+The-Gae-Bolg@users.noreply.github.com>
2021-05-02 00:28:09 -0500

    sistema de nubes mejorado

--- Assets/Scripts/CloudSystem.cs
+++ Assets/Scripts/CloudSystem.cs
@@ -2,18 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
-    /// <summary>
-    /// Performs batch translation of cloud transforms, resetting the transform position when
-    /// the cloud transform position exceeds the resetRadius distance to the controller.
-    /// Automatically collects and animates all it's child transforms.
-    /// </summary>
     public class CloudSystem : MonoBehaviour
     {
         public Vector3 windDirection = Vector2.left;
-        public float windSpeed = 1;
-        public float minSpeed = 0.5f;
-        public float resetRadius = 100;
+        public float windSpeed = 0.002f;
+        public float minSpeed = 0.001f;
+        public float resetRadius = 10;
         Transform[] clouds;
         float[] speeds;
 
@@ -30,22 +24,21 @@ using UnityEngine;
         }
 
         void Update()
-        {
-            var r2 = resetRadius * resetRadius;
+        {   //Se generan las nubes con velocidad random 
             for (var i = 0; i < speeds.Length; i++)
             {
                 var cloud = clouds[i];
                 var speed = Mathf.Lerp(minSpeed, windSpeed, speeds[i]);
                 cloud.position += windDirection * speed;
-                if (cloud.localPosition.sqrMagnitude > r2)
+                if (cloud.localPosition.x < -resetRadius - 1)//Verifica que la nube en X este fuera del radio
                 {
-                    cloud.position = -cloud.position;
-                }
-            }
+                    cloud.position = new Vector3 (transform.position.x + resetRadius, Random.Range(-resetRadius+1, resetRadius-1), 0);
+                }   //La nueva posicion X de la nube se calcula sumando el radio al centro
+            }       //Para la posicion Y se genera un numero random entre el radio negativo +1 y positivo -1 
         }
 
         void OnDrawGizmos()
         {
-            Gizmos.DrawWireSphere(transform.position, resetRadius);
+            Gizmos.DrawWireSphere(transform.position, resetRadius);// Se genera un circulo
         }
     }
